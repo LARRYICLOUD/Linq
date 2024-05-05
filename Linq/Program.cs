f6a9a177f75ba1291c64c0c96e6ed1e64b2ba26b
LARRYSALSA <75591878+LARRYICLOUD@users.noreply.github.com>
2024-05-05 11:48:29 -0500

    Agregar archivos de proyecto.

---
+++ Linq/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+var names = new List<string>()
+{
+    "Larrysalsa", "Jennifer", "Roberto", "Hugo", "Pedro"  //origen de datos
+
+
+};
+var namesResult = (from n in names
+                   orderby n ascending  //descending 
+                   select n).ToList();                 //consulta de datos
+
+
+foreach (var name in namesResult)
+{                                       //ejecucion de datos
+    Console.WriteLine(name);
+}
