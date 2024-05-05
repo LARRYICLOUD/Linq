5c045808a06df45d1173aeaa45da59ab4a224800
LARRYSALSA <75591878+LARRYICLOUD@users.noreply.github.com>
2024-05-05 12:02:50 -0500

    funcion2

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -12,6 +12,10 @@ var namesResult = (from n in names
                    select n).ToList();                 //consulta de datos
 
 
+var namesResult2 = names.Where(n=>n.Length>3 && n.Length < 5) //se puede expresar en funcion y es lo mismo
+                          . OrderByDescending(n => n);
+
+
 foreach (var name in namesResult)
 {                                       //ejecucion de datos
     Console.WriteLine(name);
